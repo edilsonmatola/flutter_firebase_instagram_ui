1e24f24ca3c914d886cbf355bf73a111186fe183
Edilson Matola <edilsonmatola13@gmail.com>
2022-01-13 23:02:28 +0200

    Added Responsive Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import 'layout/mobile_screen_layout.dart';
 import 'layout/responsive_screen_layout.dart';
 import 'layout/web_screen_layout.dart';
 import 'utils/colors_util.dart';
 
-void main() {
+Future<void> main() async {
+// !Making sure to initialize the Flutter widgets before anything
+  WidgetsFlutterBinding.ensureInitialized();
+  // kIsWeb is a constant that is true if the application was compiled to run on the web.
+  if (kIsWeb) {
+    // *Initializing Firebase for web
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCBpXigsnj9wN2PHqa7pq50mJ8mQnR3JD0',
+          appId: '1:443107625071:web:ee34a0ad55d1bafda3e59a',
+          messagingSenderId: '443107625071',
+          projectId: 'instagram-1f034',
+          storageBucket: 'instagram-1f034.appspot.com'),
+    );
+  } else {
+    // *Initializing Firebase for mobile
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
