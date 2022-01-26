7a7ab643fc8ecbfe7158de6de0d3181ee308a0d1
Edilson Matola <edilsonmatola13@gmail.com>
2022-01-26 13:20:44 +0200

    chore: Added post storage functionality

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:uuid/uuid.dart';
 
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -16,8 +17,15 @@ class StorageMethods {
     // * .ref ->  Pointer to the file in our storage (firebase), it can refer to a file that exists or does not exist.
     // * first child -> child is a folder, that can exist or not
     // * second child -> will be the user Id, so we need to get the user ID.
-    final pathReference =
+    var pathReference =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    if (isPost) {
+      // * Generate a unique ID for the post
+      final uniqueIdPost = const Uuid().v1();
+      // * The name of the post will be unique Id
+      pathReference = pathReference.child(uniqueIdPost);
+    }
 
     // * Putting the file in the firebase, in this case Data because we are using
     // Uint8List
